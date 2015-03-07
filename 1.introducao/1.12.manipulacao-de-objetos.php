18e44e163f46a06ecf0f7fab9f5b0e5425ff4a2e
Lucas Lessa <lucaslessa@me.com>
2015-03-07 02:46:57 -0300

    Finalizando o Capitulo 1

--- 1.introducao/1.12.manipulacao-de-objetos.php
+++ 1.introducao/1.12.manipulacao-de-objetos.php
@@ -5,11 +5,11 @@
 /* Cria uma nova classe chamada Funcionario */
 class Funcionario
 {
-	var $Codigo;
-	var $Nome;
-	var $Salario;
-	var $Departamento = 'Estagiário';
-
+    var $Codigo;
+    var $Nome;
+    var $Salario;
+    var $Departamento = 'Estagiário';
+    
     function SetSalario() {
     }
     function GetSalario() {
@@ -19,6 +19,7 @@ class Funcionario
     function GetNome() {
     }
 }
+
 /* get_class_methods - retorna um array com os métodos da classe */
 print_r(get_class_methods('Funcionario'));
 echo "<br/><br/>";
@@ -31,9 +32,15 @@ echo "<br/><br/>";
 $peter = new Funcionario;
 $peter->Codigo = 44;
 $peter->Nome = 'Peter Parker';
-$peter->Salario += 100;
+$peter->Salario+= 100;
 $peter->Departamento = 'Fotografia';
+
 /* get_object_vars - retorna um array com os nomes e conteúdos das propriedades do objeto */
 print_r(get_object_vars($peter));
+echo "<br/><br/>";
+
+/* get_class - retorna a classe que o objeto pertence */
+print_r(get_class($peter));
 echo "<br/>";
+
 ?>
